fb157eb0ca96d2a41be4711f9439e8535b5a84de
Joseph Yoon <jyoon@provectium.com>
2015-03-20 20:03:34 +0100

    Going back to build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Wnable error reporting to the console
+set -e
+
+# Build site with jekyll, by default to '_site' folder
+bundle exec jekyll build
+
+# Cleanup
+rm -rf ../theroyalliar.github.io.master
+
+# Clone 'master' branch of the repository using encrypted GH_TOKEN for authentication
+git clone https://${GH_TOKEN}@github.com/theroyalliar/theroyalliar.github.io.git ../theroyalliar.github.io.master
+
+# Copy generated HTML site to 'master' branch
+cp -R _site/* ../theroyalliar.github.io.master
+
+# Commit and push generated content to 'master' branch
+cd ../theroyalliar.github.io.master
+touch .nojekyll
+git config user.email "${GIT_EMAIL}"
+git config user.name "${GIT_NAME}"
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin master > /dev/null 2>&1
